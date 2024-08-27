d907cb53f04df8dd7010567ddf32ef50154fca65
Shishir19999 <shishirsharma199@gmail.com>
2024-08-27 16:14:10 +0545

    update

--- Task2/src/App.jsx
+++ Task2/src/App.jsx
@@ -1,46 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react'
+import UserList from './UserList'
 
-const UserList = () => {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    // Fetch users from API
-    const fetchUsers = async () => {
-      try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/users');
-        if (!response.ok) {
-          throw new Error('Failed to fetch users');
-        }
-        const data = await response.json();
-        setUsers(data);
-      } catch (error) {
-        setError(error.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchUsers();
-  }, []);
-
-  // Render component based on loading, error, or data state
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-
+function App() {
   return (
-    <ul>
-      {users.map((user) => (
-        <li key={user.id}>{user.name}</li>
-      ))}
-    </ul>
-  );
-};
+    <div>
+      <h1>user list</h1>
+      <UserList/>
+    </div>
+  )
+}
 
-export default UserList;
+export default App
---
+++ Task2/src/UserList.jsx
@@ -0,0 +1,34 @@
+// UserList.jsx
+import React, { useEffect, useState } from 'react';
+
+const UserList = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        if (!response.ok) {
+          throw new Error('Failed to fetch users');
+        }
+        const data = await response.json();
+        setUsers(data);
+        console.log(data)
+      } catch (error) {
+        console.log(error)
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+ return (
+    <ol>
+      {users.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ol>
+  );
+};
+
+export default UserList;
